682fdf95df3891de9451d6ddfec23991ab324ec4
Zero Zhang <southbean2013@gmail.com>
2017-09-15 20:55:49 +0800

    [Fix] scroll bugs & [Update] style

--- src/index.js
+++ src/index.js
@@ -43,16 +43,17 @@ let page = {
     })
   },
   scrollSection: function (height, duration) {
-    let gap = 20,
-        step = height * gap / duration,
+    let gap = 10,
+        step = Math.ceil(height * gap / duration),
         counter = 0,
+        coe = height > 0 ? 1 : -1,
         interval = setInterval(() => {
-          if (counter >= duration / gap) {
-            clearInterval(interval)
-          } else {
-            window.scrollBy(0, step)
+            let dist = height - counter * step
+            window.scrollBy(0, coe * step < coe * dist ? step : dist)
             counter++
-          }
+            if (coe * dist <= coe * step) {
+              clearInterval(interval)
+            }
         }, gap)
   }
 }
